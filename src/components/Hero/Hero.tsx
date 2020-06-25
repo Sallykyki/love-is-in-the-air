a5c73dec9d613f56a5585bd931e7f2bc9eb217b4
Huong Pham <huong.pq12@gmail.com>
2020-06-25 22:05:39 +0300

    feauture: add Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero: React.FunctionComponent = () => {
+  return (
+    <div className="component-Hero">
+      <div className="component-Hero__title">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
