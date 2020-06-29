7078f8b6f0edec179ae1bdc387be2e6d566eeb3e
Huong Pham <huong.pq12@gmail.com>
2020-06-29 16:12:08 +0300

    style: add animated svg to hero area

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import animatedBg from "../../assets/images/animated-bg.svg";
 
 const Hero: React.FunctionComponent = () => {
   return (
     <div className="component-Hero">
       <div className="component-Hero__title">
+        <img src={animatedBg} />
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </div>
